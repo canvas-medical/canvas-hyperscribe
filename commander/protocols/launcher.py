f8714c169bca442dc365eeb0a088205048622853
Andrew Hines <andrew.hines@canvasmedical.com>
2025-02-28 16:34:58 -0800

    merging capture app into commander

---
+++ commander/protocols/launcher.py
@@ -0,0 +1,24 @@
+from canvas_sdk.effects import Effect
+from canvas_sdk.handlers.action_button import ActionButton
+from canvas_sdk.effects.launch_modal import LaunchModalEffect
+from canvas_sdk.v1.data.note import Note
+from logger import log
+
+class HyperscribeLauncher(ActionButton):
+    BUTTON_TITLE = "🖊️ Hyperscribe"
+    BUTTON_KEY = "HYPERSCRIBE_LAUNCHER"
+    BUTTON_LOCATION = ActionButton.ButtonLocation.NOTE_HEADER
+
+    def handle(self) -> list[Effect]:
+        audio_server_base_url = self.secrets['AudioHost'].rstrip('/')
+        interval = self.secrets['AudioIntervalSeconds']
+        note_id = str(Note.objects.get(dbid=self.context['note_id']).id)
+        patient_id = self.target
+        hyperscribe_pane = LaunchModalEffect(
+            url=f"{audio_server_base_url}/capture/{patient_id}/{note_id}?interval={interval}",
+            target=LaunchModalEffect.TargetType.RIGHT_CHART_PANE
+        )
+        return [hyperscribe_pane.apply()]
+
+    def visible(self) -> bool:
+        return True
