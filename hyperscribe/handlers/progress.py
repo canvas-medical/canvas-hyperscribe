2b72079feee48dbc26abe43c559c39f8a3d93426
Andrew Hines <andrew.hines@canvasmedical.com>
2025-06-27 17:58:13 -0700

    implements audio server

--- hyperscribe/handlers/progress.py
+++ hyperscribe/handlers/progress.py
@@ -57,7 +57,7 @@ class Progress(SimpleAPIRoute):
             requests_post(
                 Authenticator.presigned_url(
                     settings.api_signing_key,
-                    f"{identification.canvas_host()}/plugin-io/api/hyperscribe/progress",
+                    f"{identification.canvas_host()}{Constants.BASE_ROUTE}/progress",
                     {"note_id": identification.note_uuid},
                 ),
                 headers={"Content-Type": "application/json"},
--- hyperscribe/handlers/tuning_launcher.py
+++ hyperscribe/handlers/tuning_launcher.py
@@ -26,12 +26,13 @@ class TuningLauncher(ActionButton):
         note_id = str(Note.objects.get(dbid=self.event.context['note_id']).id)
         patient_id = self.target
 
+        # TODO: Use Authenticator here
         ts = str(int(time()))
         hash_arg = ts + self.secrets[Constants.SECRET_API_SIGNING_KEY]
         sig = sha256(hash_arg.encode('utf-8')).hexdigest()
         params = f"note_id={note_id}&patient_id={patient_id}&interval={interval}&ts={ts}&sig={sig}"
         tuning_ui = LaunchModalEffect(
-            url=f"/plugin-io/api/hyperscribe/archive?{params}",
+            url=f"{Constants.BASE_ROUTE}/archive?{params}",
             target=LaunchModalEffect.TargetType.NEW_WINDOW,
         )
         return [tuning_ui.apply()]
